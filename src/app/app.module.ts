bcf6fe0eddb81f47f3f3ecfe31494294df4f7816
hannahnicklas <37834659+hannahnicklas@users.noreply.github.com>
2018-04-03 19:26:40 +0200

    angelegt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { MessageService } from './message.service';
 
 
 @NgModule({
@@ -12,7 +13,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [MessageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
