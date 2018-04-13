86b708d64cf0b067a8cff4ada6a96068bee81edc
Vincent Brunner <vingeobrun@googlemail.com>
2018-04-13 11:21:33 +0200

    SCSS

--- src/app/internship/internship.component.ts
+++ src/app/internship/internship.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-internship',
   templateUrl: './internship.component.html',
-  styleUrls: ['./internship.component.css']
+  styleUrls: ['./internship.component.scss']
 })
 export class InternshipComponent implements OnInit {
 
--- src/app/main-nav/main-nav.component.ts
+++ src/app/main-nav/main-nav.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-main-nav',
   templateUrl: './main-nav.component.html',
-  styleUrls: ['./main-nav.component.css']
+  styleUrls: ['./main-nav.component.scss']
 })
 export class MainNavComponent implements OnInit {
 
