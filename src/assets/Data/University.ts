fc1e34a609f244be649492b5db652f942603299b
ChristopherSenn <38063779+ChristopherSenn@users.noreply.github.com>
2018-04-16 16:06:19 +0200

    final

--- src/assets/Data/University.ts
+++ src/assets/Data/University.ts
@@ -1,5 +1,8 @@
+import { Experience } from './Experience';
 
 export class University {
     id: number;
     name: string;
+    experiences: Experience[
+    ];
   }
--- src/assets/Data/university-data.service.ts
+++ src/assets/Data/university-data.service.ts
@@ -3,7 +3,10 @@ import { InMemoryDbService } from 'angular-in-memory-web-api';
 export class UniversityDataService implements InMemoryDbService {
   createDb() {
     const unis = [
-        { id: 1, name: 'University of Technology' },
+        { id: 1, name: 'University of Technology', experiences: [
+          {student: 'John Doe', text: 'I hated it!'},
+          {student: 'Max Mustermann', text: 'I loved it!'}
+        ] },
         { id: 2, name: 'Technische Hochschule Ingolstadt' }
     ];
     return {unis};
