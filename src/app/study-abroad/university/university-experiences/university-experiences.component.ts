5154a2a8ff83bc49de93740b8ebab0ac98f9bf11
ChristopherSenn <38063779+ChristopherSenn@users.noreply.github.com>
2018-04-17 12:50:25 +0200

    a

--- src/app/study-abroad/university/university-experiences/university-experiences.component.ts
+++ src/app/study-abroad/university/university-experiences/university-experiences.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { UniversityService } from '../university.service';
+import { University } from '../../../../assets/Data/University';
 
 @Component({
   selector: 'app-university-experiences',
   templateUrl: './university-experiences.component.html',
   styleUrls: ['./university-experiences.component.scss']
 })
-export class UniversityExperiencesComponent implements OnInit {
 
-  constructor() { }
+export class UniversityExperiencesComponent implements OnInit {
+  @Input() university: University;
+
+  constructor(
+    private route: ActivatedRoute,
+    private universityService: UniversityService,
+    private location: Location,
+    private router: Router
+    ) { }
 
   ngOnInit() {
+    this.getUniversity();
+  }
+
+  getUniversity(): void {
+     const id = +this.route.parent.snapshot.paramMap.get('id');
+     this.universityService.getUniversity(id)
+      .subscribe(university => this.university = university);
   }
 
 }
--- src/assets/Data/university-data.service.ts
+++ src/assets/Data/university-data.service.ts
@@ -4,10 +4,13 @@ export class UniversityDataService implements InMemoryDbService {
   createDb() {
     const unis = [
         { id: 1, name: 'University of Technology', experiences: [
-          {student: 'John Doe', text: 'I hated it!'},
-          {student: 'Max Mustermann', text: 'I loved it!'}
+          {student: 'John Doe1', text: 'I hated it!'},
+          {student: 'Max Mustermann1', text: 'I loved it!'}
         ] },
-        { id: 2, name: 'Technische Hochschule Ingolstadt' }
+        { id: 2, name: 'Technische Hochschule Ingolstadt', experiences: [
+          {student: 'John Doe2', text: 'I hated it!'},
+          {student: 'Max Mustermann2', text: 'I loved it!'}
+        ] }
     ];
     return {unis};
   }
