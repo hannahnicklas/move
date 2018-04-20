b438fec1821a9d9a2a2cd934f549fc1de994ee39
Vanessa Straube <straubevanessa@gmail.com>
2018-04-20 10:54:12 +0200

    Consider 3 Spalten

--- src/app/study-abroad/university/university-consider/university-consider.component.ts
+++ src/app/study-abroad/university/university-consider/university-consider.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { UniversityService } from '../university.service';
+import { University } from '../../../../assets/Data/University';
 
 @Component({
   selector: 'app-university-consider',
@@ -6,10 +10,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./university-consider.component.scss']
 })
 export class UniversityConsiderComponent implements OnInit {
+  @Input() university: University;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private universityService: UniversityService,
+    private location: Location,
+    private router: Router
+    ) { }
 
   ngOnInit() {
+    this.getUniversity();
   }
 
+  getUniversity(): void {
+     const id = +this.route.parent.snapshot.paramMap.get('id');
+     this.universityService.getUniversity(id)
+      .subscribe(university => this.university = university);
+  }
 }
