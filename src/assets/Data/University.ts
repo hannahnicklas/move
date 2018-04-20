5498e2c63cab3380972f747f0ee8ab130d698099
ChristopherSenn <38063779+ChristopherSenn@users.noreply.github.com>
2018-04-20 10:51:25 +0200

    update data

--- src/assets/Data/University.ts
+++ src/assets/Data/University.ts
@@ -3,6 +3,10 @@ import { Experience } from './Experience';
 export class University {
     id: number;
     name: string;
+    country: string;
+    city: string;
+    descriptionText: string;
+    pictures: string[];
     experiences: Experience[
     ];
   }
