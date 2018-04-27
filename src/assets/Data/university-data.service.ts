58bd5dbcb49c80dd4f6666fbea8c197e33b7cf74
ChristopherSenn <38063779+ChristopherSenn@users.noreply.github.com>
2018-04-27 08:19:22 +0200

    a

--- src/assets/Data/university-data.service.ts
+++ src/assets/Data/university-data.service.ts
@@ -3,33 +3,33 @@ import { InMemoryDbService } from 'angular-in-memory-web-api';
 export class UniversityDataService implements InMemoryDbService {
   createDb() {
     const unis = [
-        { id: 1, name: 'University of Technology', country: 'Australia', city: 'Sydney', descriptionText: 'Lorem Ipsum', pictures: [
-          {path: '../../sadsa' },
-          {path: '../../sadsad' }
-        ] , experiences: [
-          {student: 'John Doe1', course: 'DB (THI)', text: 'I hated it!'},
-          {student: 'Dora Delighted1', course: 'UXD (THI)', text: 'I loved it!'}
-        ] , facts: [
-          {icon: 'far fa-square', title: 'Visa', description: 'Student visa. Visa needed.'},
-          {icon: 'far fa-square', title: 'Left driving traffic', description: 'Like in Great Britain'}
-        ] },
+        { id: 1, name: 'University of Technology', country: 'Australia', city: 'Sydney', descriptionText: 'Lorem Ipsum',
+        picturePath: '../images/app-component/university-general',
+          experiences: [
+            {student: 'John Doe1', course: 'DB (THI)', text: 'I hated it!'},
+            {student: 'Dora Delighted1', course: 'UXD (THI)', text: 'I loved it!'}
+          ] , facts: [
+            {icon: 'far fa-square', title: 'Visa', description: 'Student visa. Visa needed.'},
+            {icon: 'far fa-square', title: 'Left driving traffic', description: 'Like in Great Britain'}
+          ] },
         { id: 2, name: 'Technische Hochschule Ingolstadt', country: 'Germany', city: 'Ingolstadt',
-        descriptionText: 'Lorem Ipsum2', pictures: [
-          {path: '../../sadsa2' },
-          {path: '../../sadsad2' }
-        ] , experiences: [
-          {student: 'Dora Delighted', course: 'User Experience Design (THI)',
-          text: 'WiSe, 24.07.2016 – 18.12.2016 Visual Communication (UTS)'},
-          {student: 'Felix Fortunate', course: 'Software Engineering (THI)',
-          text: 'WiSe, 24.07.2016 – 18.12.2016 Software Development (UTS)'},
-          {student: 'Tanja Bayer', course: 'User Experience Design (THI)',
-          text: 'WiSe, 24.07.2016 – 18.12.2016 Software Development (UTS)'},
-          {student: 'Hillarious Happy', course: 'Engineering (THI)',
-          text: 'WiSe, 24.07.2016 – 18.12.2016 Engineering (UTS)'}
-        ] , facts: [
-          {icon: 'far fa-square', title: 'No Visa', description: 'No Visa needed.'},
-          {icon: 'far fa-square', title: 'No Left driving traffic', description: 'Its Germany'}
-        ] }
+          descriptionText: 'Lorem Ipsum2', pictures: [
+            {path: '../../sadsa2' },
+            {path: '../../sadsad2' }
+          ] , experiences: [
+            {student: 'Dora Delighted', course: 'User Experience Design (THI)',
+              text: 'WiSe, 24.07.2016 – 18.12.2016 Visual Communication (UTS)'},
+            {student: 'Felix Fortunate', course: 'Software Engineering (THI)',
+              text: 'WiSe, 24.07.2016 – 18.12.2016 Software Development (UTS)'},
+            {student: 'Tanja Bayer', course: 'User Experience Design (THI)',
+              text: 'WiSe, 24.07.2016 – 18.12.2016 Software Development (UTS)'},
+            {student: 'Hillarious Happy', course: 'Engineering (THI)',
+              text: 'WiSe, 24.07.2016 – 18.12.2016 Engineering (UTS)'}
+          ] , facts: [
+            {icon: 'far fa-square', title: 'No Visa', description: 'No Visa needed.'},
+            {icon: 'far fa-square', title: 'No Left driving traffic', description: 'Its Germany'}
+          ]
+        }
     ];
 
     const students = [
