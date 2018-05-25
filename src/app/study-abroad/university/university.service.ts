92f1c482fe726a3aebe47b702278768c57df6b55
NahanTu <tuo8465@thi.de>
2018-05-25 10:20:43 +0200

    datenbank uni

--- src/app/study-abroad/university/university.service.ts
+++ src/app/study-abroad/university/university.service.ts
@@ -21,6 +21,7 @@ export class UniversityService {
     return this.http.get<University>(url);
   }
 
+  // hier noch die richtige ID bekommen Implementieren
   getUniversitiesForStudent(ids: number[]) {
     console.log(ids);
     return this.http.get<University[]>(this.unisUrl);
